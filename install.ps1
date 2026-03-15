5727d6069e6bb8d370db9243f928acddc8979819
erikvk <erik.von.krusenstierna@gmail.com>
2026-03-15 22:29:36 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -17,7 +17,7 @@ function Write-Banner {
     $line5 = "|| || || ||   ___     __      _      _     __           __   "
     $line5 += $VersionTag.PadLeft(7)
     $line5 += "   /"
-    Write-Host "____________________________________________________________________________"
+    Write-Host "________________________________________________________________________"
     Write-Host "                 ___                   __             __               /"
     Write-Host "                / _ )_______  ___ ____/ /______ ____ / /____ ____      \"
     Write-Host "   |\ |\       / _  / __/ _ \/ _ ``/ _  / __/ _ ``(_-</ __/ -_) __/      /"
@@ -25,7 +25,7 @@ function Write-Banner {
     Write-Host $line5
     Write-Host "\| || || \|  / _ |___/ /_ _  (_)__  (_)__ / /________ _/ /____  ____   \"
     Write-Host "   \| \|    / __ / _  /  ' \/ / _ \/ (_-</ __/ __/ _ ``/ __/ _ \/ __/   /"
-    Write-Host "           /_/ |_\_,_/_/_/_/_/_//_/_/___/\__/_/  \_,_/\__/\___/_/     \"
+    Write-Host "           /_/ |_\_,_/_/_/_/_/_//_/_/___/\__/_/  \_,_/\__/\___/_/      \"
     Write-Host "          <>------------------------------------------------------<>   /"
     Write-Host "_______________________________________________________________________\"
     Write-Host
@@ -53,24 +53,42 @@ try {
     Write-Host "> Detected platform: $platform ($arch)"
     Write-Host "> Latest version: $version"
 
-    $asset = $release.assets | Where-Object { $_.name -match 'setup\.exe$' } | Select-Object -First 1
+    $assets = @($release.assets)
+    $asset = $assets | Where-Object { $_.name -match 'setup\.exe$' } | Select-Object -First 1
 
     if (-not $asset) {
-        throw "Could not find the Windows installer in release $version."
+        $asset = $assets |
+            Where-Object { $_.name -match '\.exe$' -and $_.name -notmatch '\.blockmap$' } |
+            Select-Object -First 1
+    }
+
+    if (-not $asset) {
+        $asset = $assets | Where-Object { $_.name -match '\.msi$' } | Select-Object -First 1
+    }
+
+    if (-not $asset) {
+        $assetNames = if ($assets.Count -gt 0) { ($assets | ForEach-Object { $_.name }) -join ', ' } else { '<none>' }
+        throw "Could not find a Windows installer in release $version. Assets: $assetNames"
     }
 
     $downloadUrl = $asset.browser_download_url
-    $tmpFile = Join-Path $env:TEMP "bcadmin-setup.exe"
+    $fileName = if ($asset.name) { $asset.name } else { 'bcadmin-installer.exe' }
+    $tmpFile = Join-Path $env:TEMP $fileName
 
-    Write-Step "Pulling the $appName installer from GitHub"
+    Write-Step "Pulling the $appName installer from GitHub ($fileName)"
     Invoke-WebRequest -Uri $downloadUrl -OutFile $tmpFile -UseBasicParsing
     Write-Done
 
     Write-Step "Running the installer"
-    $installer = Start-Process -FilePath $tmpFile -Wait -PassThru
+    if ($tmpFile -match '\.msi$') {
+        $installer = Start-Process -FilePath "msiexec.exe" -ArgumentList @('/i', $tmpFile) -Wait -PassThru
+    }
+    else {
+        $installer = Start-Process -FilePath $tmpFile -Wait -PassThru
+    }
     Write-Done
 
-    if ($installer.ExitCode -ne 0) {
+    if ($installer.ExitCode -notin @(0, 1641, 3010)) {
         throw "The installer exited with code $($installer.ExitCode)."
     }
 
